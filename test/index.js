0988c9277617c97a77f70b9b4b29e0ee9b604d5e
coderk <363959579@qq.com>
2018-07-26 11:00:28 +0800

    feat: 完善demo测试工程

--- test/index.js
+++ test/index.js
@@ -1,40 +1,61 @@
-const TestService = rce.extend(function (_super) {
+// 创建服务
+const Service = rce.extend(function (_super) {
     _super();
+    // 监听 Notice
     this.__ln('to_service', (notice) => {
-        console.log('service catch: ', notice)
-        this.__sb('some broadcast', 1)
+        console.log('---- service caught notice data ----')
+        console.log(notice.data)
+        console.log('---- service send broadcast ----')
+        this.__sb('to_component', `I caught an notice data: ${notice.data}`)
     })
-}, rce.Service, 'TestService')
+}, rce.Service, 'Service')
 
-const TestService2 = rce.extend(function (_super) {
-    _super();
-    this.__ln('to_service2', (notice) => {
-        console.log('service2 catch: ', notice)
-        this.__sb('some broadcast2', 2)
-    })
-}, rce.Service, 'TestService2')
-
-const service1 = new TestService();
-const service2 = new TestService2();
-
+// 创建app
 const app = new rce.App();
-app.useService(service1, service2);
 
-// 挂载Vue插件
+// 挂载 service 实例
+app.useService(new Service());
+
+// 使用 Vue 插件为 Vue 实例及其组件赋能
 Vue.use(app.viewPlugin('Vue'))
 
-const vm = new Vue({
-    el: '#app'
+// 定义组件
+Vue.component('my-button', {
+    props: ['label', 'index'],
+    template: `
+        <button @click='sendTo'>{{label}}</button>
+    `,
+    created: function() {
+        // 组件监听广播
+        this.__lb('to_component', function(broadcast) {
+            console.log(`---- component ${this.index} caught broadcast data ----`);
+            console.log(broadcast.data);
+        }, this)
+    },
+    methods: {
+        sendTo: function() {
+            // 组件发送 Notice
+            console.log(`---- component ${this.index} send notice ----`);
+            this.__sn('to_service', this.label);
+        }
+    }
 })
 
+const vm = new Vue({
+    el: '#app',
+    template: `<div id='app'>
+        <my-button index='1' label='hello'></my-button>
+        <my-button index='2' label='world'></my-button>
+    </div>`
+})
+
+// 启动App
 app.start().then(() => {
-    // 监听广播事件
-    vm.__lb('some broadcast2', (broadcast) => {
-        console.log('vm catch broadcast: ', broadcast)
+    vm.__lb('to_vm', (broadcast) => {
+        console.log('---- vm caught broadcast data ----');
+        console.log(broadcast.data);
     })
-    vm.__lb('some broadcast', (broadcast) => {
-        console.log('vm catch broadcast: ', broadcast)
-    })
-    // 视图层发送通知
-    vm.__sn('to_service');
+    console.log('---- vm send notice ----');
+    // Vue 实例发送 Notice
+    vm.__sn('to_service', 'I am vm');
 })
